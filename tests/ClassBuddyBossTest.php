2654734c4204de508d244c5c0609910497237ae8
aradovic <aleksandar.radovic964@gmail.com>
2021-01-15 20:30:40 +0100

    added github workflow and test

---
+++ tests/ClassBuddyBossTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use wpCloud\StatelessMedia\WPStatelessStub;
+use WPSL\BuddyBoss;
+
+/**
+ * Class ClassBuddyBossTest
+ */
+
+class ClassBuddyBossTest extends TestCase {
+
+  public static $functions;
+
+  public function setUp(): void {
+    self::$functions = $this->createPartialMock(
+      ClassBuddyBossTest::class,
+      ['add_filter', 'add_action', 'apply_filters', 'do_action']
+    );
+
+    $this::$functions->method('apply_filters')->will($this->returnArgument(1));
+  }
+
+  public function testShouldInitModule() {
+
+  }
+
+}
+
+function ud_get_stateless_media() {
+  return WPStatelessStub::instance();
+}
