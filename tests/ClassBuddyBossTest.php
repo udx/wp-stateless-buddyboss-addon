e011315daf3a0fd8ebaff7caae00360758a3e16c
vladimir <plan.vova@gmail.com>
2021-02-16 14:34:58 +0200

    added tests

--- tests/ClassBuddyBossTest.php
+++ tests/ClassBuddyBossTest.php
@@ -1,9 +1,10 @@
 <?php
 
+namespace WPSL\BuddyBoss;
+
 use PHPUnit\Framework\TestCase;
 use wpCloud\StatelessMedia\WPStatelessStub;
-use wpCloud\StatelessMedia\Compatibility;
-use WPSL\BuddyBoss;
+use WPSL\BuddyBoss\BuddyBoss;
 
 /**
  * Class ClassBuddyBossTest
@@ -16,16 +17,47 @@ class ClassBuddyBossTest extends TestCase {
   public function setUp(): void {
     self::$functions = $this->createPartialMock(
       ClassBuddyBossTest::class,
-      ['add_filter', 'add_action', 'apply_filters', 'do_action']
+      ['add_filter', 'apply_filters']
     );
 
     $this::$functions->method('apply_filters')->will($this->returnArgument(1));
   }
 
   public function testShouldInitModule() {
+    self::$functions->expects($this->exactly(1))
+      ->method('add_filter')
+      ->with('stateless_skip_cache_busting');
 
+    $budyboss = new BuddyBoss();
+    $budyboss->module_init([]);
   }
 
+  public function testShouldSkipCacheBusting() {
+    $budyboss = new BuddyBoss();
+
+    $this->assertEquals('https://test.test/buddyboss-platform/test', $budyboss->skip_cache_busting(null, 'https://test.test/buddyboss-platform/test'));
+  }
+
+  public function add_filter() {
+  }
+
+  public function apply_filters() {
+  }
+
+  public function debug_backtrace($a, $b) {
+  }
+}
+
+function add_filter($a, $b, $c = 10, $d = 1) {
+  return ClassBuddyBossTest::$functions->add_filter($a, $b, $c, $d);
+}
+
+function apply_filters($a, $b) {
+  return ClassBuddyBossTest::$functions->apply_filters($a, $b);
+}
+
+function strpos() {
+  return true;
 }
 
 function ud_get_stateless_media() {
